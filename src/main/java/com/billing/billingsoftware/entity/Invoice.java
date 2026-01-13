c0dcf0b1e68b757c6fd8b6527abf3a1fab60d070
Sakshi Darokar <164064604+Sakshi-Darokar@users.noreply.github.com>
2026-01-13 16:00:55 +0530

    Update Invoice.java

--- src/main/java/com/billing/billingsoftware/entity/Invoice.java
+++ src/main/java/com/billing/billingsoftware/entity/Invoice.java
@@ -3,47 +3,16 @@ package com.billing.billingsoftware.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
-/*
- THIS IS INVOICE ENTITY CLASS.
- THIS CLASS REPRESENTS THE INVOICE TABLE STRUCTURE.
-
- IT STORES COMPLETE BILL INFORMATION LIKE:
- CUSTOMER DETAILS, PRODUCT LIST, TOTAL AMOUNT, TAX, DISCOUNT AND FINAL AMOUNT.
-*/
-
 public class Invoice {
-
-    // AUTO GENERATED UNIQUE INVOICE NUMBER
     private Long invoiceId;
-
-    // DATE AND TIME WHEN INVOICE IS GENERATED
     private LocalDateTime invoiceDate;
-
-    // CUSTOMER FOR WHOM THIS INVOICE IS GENERATED
     private Customer customer;
-
-    // LIST OF PRODUCTS INCLUDED IN THIS INVOICE
     private List<InvoiceItem> items;
-
-    // TOTAL AMOUNT WITHOUT TAX
     private double totalAmount;
-
-    // TOTAL GST AMOUNT
     private double totalTax;
-
-    // DISCOUNT APPLIED ON BILL
     private double discount;
-
-    // FINAL PAYABLE AMOUNT (TOTAL + TAX - DISCOUNT)
     private double finalAmount;
-
-    /*
-     DEFAULT CONSTRUCTOR
-     REQUIRED BY SPRING FOR OBJECT CREATION
-     */
     public Invoice() {}
-
-    // GETTER AND SETTER FOR INVOICE ID
     public Long getInvoiceId() {
         return invoiceId;
     }
@@ -51,8 +20,6 @@ public class Invoice {
     public void setInvoiceId(Long invoiceId) {
         this.invoiceId = invoiceId;
     }
-
-    // GETTER AND SETTER FOR INVOICE DATE
     public LocalDateTime getInvoiceDate() {
         return invoiceDate;
     }
@@ -61,7 +28,6 @@ public class Invoice {
         this.invoiceDate = invoiceDate;
     }
 
-    // GETTER AND SETTER FOR CUSTOMER
     public Customer getCustomer() {
         return customer;
     }
@@ -79,7 +45,6 @@ public class Invoice {
         this.items = items;
     }
 
-    // GETTER AND SETTER FOR TOTAL AMOUNT
     public double getTotalAmount() {
         return totalAmount;
     }
@@ -96,8 +61,6 @@ public class Invoice {
     public void setTotalTax(double totalTax) {
         this.totalTax = totalTax;
     }
-
-    // GETTER AND SETTER FOR DISCOUNT
     public double getDiscount() {
         return discount;
     }
@@ -106,7 +69,6 @@ public class Invoice {
         this.discount = discount;
     }
 
-    // GETTER AND SETTER FOR FINAL PAYABLE AMOUNT
     public double getFinalAmount() {
         return finalAmount;
     }
