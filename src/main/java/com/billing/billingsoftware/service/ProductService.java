501c0ea2e804ec117023a774af4de970b6294f57
Sakshi Darokar <164064604+Sakshi-Darokar@users.noreply.github.com>
2026-01-13 16:21:40 +0530

    Update ProductService.java

--- src/main/java/com/billing/billingsoftware/service/ProductService.java
+++ src/main/java/com/billing/billingsoftware/service/ProductService.java
@@ -6,72 +6,39 @@ import com.billing.billingsoftware.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-/*
- THIS IS PRODUCT SERVICE CLASS.
- SERVICE LAYER CONTAINS BUSINESS LOGIC RELATED TO PRODUCTS.
-
- CONTROLLER CALLS THIS CLASS.
- THIS CLASS COMMUNICATES WITH REPOSITORY TO SAVE AND FETCH DATA.
-*/
-
-@Service   // TELLS SPRING THIS CLASS IS A SERVICE LAYER COMPONENT
+@Service 
 public class ProductService {
-
-    // REPOSITORY OBJECT USED TO ACCESS PRODUCT DATA
+ 
     private final ProductRepository repo;
-
-    /*
-     CONSTRUCTOR INJECTION
-     SPRING WILL AUTOMATICALLY PROVIDE PRODUCT REPOSITORY OBJECT
-     */
     public ProductService(ProductRepository repo) {
         this.repo = repo;
     }
 
-    /*
-     ADD NEW PRODUCT INTO DATABASE
-     */
     public Product addProduct(Product product) {
         return repo.save(product);
     }
 
-    /*
-     UPDATE EXISTING PRODUCT DETAILS USING PRODUCT ID
-     */
     public Product updateProduct(Long id, Product product) {
 
-        // CHECK IF PRODUCT EXISTS
         Product existing = repo.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
-
-        // UPDATE PRODUCT DETAILS
         existing.setName(product.getName());
         existing.setPrice(product.getPrice());
         existing.setGstPercentage(product.getGstPercentage());
         existing.setStockQuantity(product.getStockQuantity());
 
-        // SAVE UPDATED PRODUCT
         return repo.save(existing);
     }
 
-    /*
-     FETCH ALL PRODUCTS FROM DATABASE
-     */
     public List<Product> getAllProducts() {
         return repo.findAll();
     }
 
-    /*
-     FETCH SINGLE PRODUCT USING PRODUCT ID
-     */
     public Product getProductById(Long id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
     }
 
-    /*
-     DELETE PRODUCT USING PRODUCT ID
-     */
     public void deleteProduct(Long id) {
         repo.deleteById(id);
     }
