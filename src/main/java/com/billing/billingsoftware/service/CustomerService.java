b01c1ddd9a7ae7ed40dd018936adac583a3f01d2
Sakshi Darokar <164064604+Sakshi-Darokar@users.noreply.github.com>
2026-01-13 16:17:09 +0530

    Update CustomerService.java

--- src/main/java/com/billing/billingsoftware/service/CustomerService.java
+++ src/main/java/com/billing/billingsoftware/service/CustomerService.java
@@ -6,46 +6,22 @@ import com.billing.billingsoftware.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-/*
- THIS IS CUSTOMER SERVICE CLASS.
- SERVICE LAYER CONTAINS BUSINESS LOGIC.
-
- CONTROLLER CALLS THIS CLASS AND THIS CLASS
- COMMUNICATES WITH REPOSITORY FOR DATA OPERATIONS.
-*/
-
-@Service   // TELLS SPRING THIS CLASS CONTAINS BUSINESS LOGIC
+@Service 
 public class CustomerService {
 
-    // REPOSITORY OBJECT USED TO ACCESS CUSTOMER DATA
     private final CustomerRepository repo;
-
-    /*
-     CONSTRUCTOR INJECTION
-     SPRING WILL AUTOMATICALLY PROVIDE CustomerRepository OBJECT
-     */
     public CustomerService(CustomerRepository repo) {
         this.repo = repo;
     }
-
-    /*
-     ADD NEW CUSTOMER INTO DATABASE
-     */
+ 
     public Customer addCustomer(Customer customer) {
         return repo.save(customer);
     }
 
-    /*
-     FETCH ALL CUSTOMERS FROM DATABASE
-     */
     public List<Customer> getAllCustomers() {
         return repo.findAll();
     }
 
-    /*
-     FETCH CUSTOMER BY CUSTOMER ID
-     IF CUSTOMER NOT FOUND, THROW CUSTOM EXCEPTION
-     */
     public Customer getCustomerById(Long id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
