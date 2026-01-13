8ed8301a8007dbab346ca45cbf5637b85ec90b5d
Sakshi Darokar <164064604+Sakshi-Darokar@users.noreply.github.com>
2026-01-13 16:07:54 +0530

    Clean up comments in ResourceNotFoundException
    Removed unnecessary comments from ResourceNotFoundException class.

--- src/main/java/com/billing/billingsoftware/exception/ResourceNotFoundException.java
+++ src/main/java/com/billing/billingsoftware/exception/ResourceNotFoundException.java
@@ -1,22 +1,7 @@
 package com.billing.billingsoftware.exception;
 
-/*
- THIS IS CUSTOM EXCEPTION CLASS FOR RESOURCE NOT FOUND.
-
- THIS EXCEPTION IS THROWN WHEN:
- - CUSTOMER ID IS NOT FOUND
- - PRODUCT ID IS NOT FOUND
- - INVOICE ID IS NOT FOUND
-
- THIS HELPS TO SEND PROPER ERROR MESSAGE TO CLIENT
- INSTEAD OF SHOWING CONFUSING SERVER ERROR.
-*/
-
 public class ResourceNotFoundException extends RuntimeException {
 
-    /*
-     CONSTRUCTOR TO PASS CUSTOM ERROR MESSAGE
-     */
     public ResourceNotFoundException(String message) {
         super(message);
     }
